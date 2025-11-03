e5ac7dd709da90faeb281d9a48dfedb7c9c0156f
riwoo0902 <imliu4342@gmail.com>
2025-11-03 22:21:29 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 1864 ms, Memory: 9100 KB -BaekjoonHub

--- 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.cs
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.cs
@@ -1,22 +1,15 @@
-internal class Program
+int a = int.Parse(Console.ReadLine());
+int[] arr = new int[a];
+for (int i = 0; i < a; i++) arr[i] = int.Parse(Console.ReadLine());
+long sum = 0;
+for (int i = 0; i < a; i++)
 {
-    static void Main()
+    int SeeCount = 0;
+    for (int j = i + 1; j < a; j++)
     {
-        var sr = new StreamReader(Console.OpenStandardInput());
-        int a = int.Parse(sr.ReadLine());
-        int[] arr = new int[a];
-        for (int i = 0; i < a; i++) arr[i] = int.Parse(sr.ReadLine());
-        long sum = 0;
-        for (int i = 0; i < a; i++)
-        {
-            int SeeCount = 0;
-            for(int j = i+1; j < a; j++)
-            {
-                if (arr[i] > arr[j]) SeeCount++;
-                else break;
-            }
-            sum += SeeCount;
-        }
-        Console.Write(sum);
+        if (arr[i] > arr[j]) SeeCount++;
+        else break;
     }
+    sum += SeeCount;
 }
+Console.Write(sum);
