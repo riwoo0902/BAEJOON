718c58e98f20d30a620eeb127b6b3b56fbc0e5e4
riwoo0902 <imliu4342@gmail.com>
2026-01-06 23:27:07 +0900

    [Bronze II] Title: Hashing, Time: 52 ms, Memory: 5628 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.cs
@@ -0,0 +1,24 @@
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        int length = int.Parse(Console.ReadLine());
+        string input = Console.ReadLine();
+
+        BigInteger value = 0;
+        for (int i = 0; i < length; i++) value += (input[i] - 96) * Pow(i);
+        Console.WriteLine(value);
+    }
+
+    static BigInteger Pow(int powValue)
+    {
+        BigInteger bigInteger = 1;
+        for (int i = 0; i < powValue; i++)
+        {
+            bigInteger *= 31;
+        }
+        return bigInteger;
+    }
+}
