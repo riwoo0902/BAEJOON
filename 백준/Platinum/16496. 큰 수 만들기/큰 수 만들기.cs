5e221266659482059f64002cafea818cf5752c2d
임리우 <imliu4342@gmail.com>
2026-03-26 08:39:48 +0900

    [Platinum V] Title: 큰 수 만들기, Time: 56 ms, Memory: 7108 KB -BaekjoonHub

--- 백준/Platinum/16496. 큰 수 만들기/큰 수 만들기.cs
+++ 백준/Platinum/16496. 큰 수 만들기/큰 수 만들기.cs
@@ -1,20 +1,7 @@
-internal class Program
-{
-    static void Main()
-    {
-        var sr = new StreamReader(Console.OpenStandardInput());
-        var sw = new StreamWriter(Console.OpenStandardOutput());
-        int a = int.Parse(sr.ReadLine());
-        string[] numbers = sr.ReadLine().Split();
-        Array.Sort(numbers, (x, y) => {
-            string xy = x + y;
-            string yx = y + x;
-            return string.Compare(yx, xy);
-        });
-        string result = string.Join("", numbers);
-        if (result.All(c => c == '0'))
-            result = "0";
-        sw.WriteLine(result);
-        sw.Flush();
-    }
-}
+var sr = new StreamReader(Console.OpenStandardInput());
+int a = int.Parse(sr.ReadLine());
+string[] numbers = sr.ReadLine().Split();
+Array.Sort(numbers, (x, y) => string.Compare(y + x, x + y));
+string result = string.Join("", numbers);
+if (result.All(c => c == '0'))result = "0";
+Console.WriteLine(result);
