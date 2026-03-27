19fe1d479642844aee2855607c9c20d8fd502af0
임리우 <imliu4342@gmail.com>
2026-03-28 06:05:52 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 384 ms, Memory: 59324 KB -BaekjoonHub

---
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.cs
@@ -0,0 +1,72 @@
+using System.Numerics;
+using System.Text;
+class Program
+{
+    public static Vector2[] dirs = { new Vector2(1, 0), new Vector2(0, 1), new Vector2(-1, 0), new Vector2(0, -1) };
+    static void Main()
+    {
+        StreamReader sr = new StreamReader(Console.OpenStandardInput());
+
+        int[] input = Array.ConvertAll(sr.ReadLine().Split(" "),int.Parse);
+
+        Dictionary<Vector2, Point> map = new();
+        Queue<Vector2> stack = new();
+        HashSet<Vector2> hash = new();
+        for(int i = 0; i < input[0]; i++)
+        {
+            int[] input2 = Array.ConvertAll(sr.ReadLine().Split(" "), int.Parse);
+            for (int j = 0; j < input[1]; j++)
+            {
+                map.Add(new Vector2(i, j), new Point(input2[j]));
+                if (input2[j] == 2)
+                {
+                    map[new Vector2(i, j)].moveCount = 0;
+                    stack.Enqueue(new Vector2(i, j));
+                    hash.Add(new Vector2(i, j));
+                }
+                else if (input2[j] == 0)
+                {
+                    map[new Vector2(i, j)].moveCount = 0;
+                    hash.Add(new Vector2(i, j));
+                }
+            }
+        }
+
+        while (stack.Count > 0)
+        {
+            Vector2 currentPointPos = stack.Dequeue();
+            Point currentPoint = map[currentPointPos];
+            foreach (Vector2 dir in dirs)
+            {
+                Vector2 dirPos = currentPointPos + dir;
+                if (hash.Contains(dirPos)) continue;
+                if(map.TryGetValue(dirPos,out Point point))
+                {
+                    point.moveCount = currentPoint.moveCount + 1;
+                    hash.Add(dirPos);
+                    stack.Enqueue(dirPos);
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < input[0]; i++)
+        {
+            for (int j = 0; j < input[1]; j++)
+            {
+                sb.Append($"{map[new Vector2(i, j)].moveCount} ");
+            }
+            sb.AppendLine();
+        }
+        Console.WriteLine(sb.ToString());
+    }
+}
+public class Point
+{
+    public int value;
+    public int moveCount = -1;
+    public Point(int value)
+    {
+        this.value = value;
+    }
+}
