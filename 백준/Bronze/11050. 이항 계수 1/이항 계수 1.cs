776a7e7b6a2d064babfa88b719217b3cdeaa8541
임리우 <imliu4342@gmail.com>
2026-03-28 14:05:02 +0900

    [Bronze I] Title: 이항 계수 1, Time: 52 ms, Memory: 5516 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.cs
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.cs
@@ -8,7 +8,7 @@ class Program
         int value2 = 1;
         for (int i = 1; i <= a[1]; i++) value2 *= i;
         int value3 = 1;
-        for (int i = 1; i <= a[0] - a[1]; i++) value2 *= i;
+        for (int i = 1; i <= a[0] - a[1]; i++) value3 *= i;
         Console.WriteLine(value1 / (value2 * value3));
     }
 }
