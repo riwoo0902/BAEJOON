0904871ae8bed5f4dcde89882ef5a811ad46d6f6
임리우 <imliu4342@gmail.com>
2026-04-11 23:51:05 +0900

    [Gold IV] Title: 뒤집기 3, Time: 48 ms, Memory: 5376 KB -BaekjoonHub

--- 백준/Gold/1464. 뒤집기 3/뒤집기 3.cs
+++ 백준/Gold/1464. 뒤집기 3/뒤집기 3.cs
@@ -1,4 +1,14 @@
 string input = Console.ReadLine();
 string value = input[0].ToString();
-for(int i = 1; i < input.Length; i++) value = value[0] < input[i] ? value + input[i] : input[i] + value;
+for(int i = 1; i < input.Length; i++)
+{
+    if (value[0] < input[i])
+    {
+        value += input[i];
+    }
+    else
+    {
+        value = input[i] + value;
+    }
+}
 Console.WriteLine(value);
